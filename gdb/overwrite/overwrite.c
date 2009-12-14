d812496da776750491db0e37f2ba481ae9d0012a
Joel Sherrill <joel.sherrill@OARcorp.com>
2009-12-14 13:07:19 +0000

    2009-12-14	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* ChangeLog, overwrite/.cvsignore, overwrite/Makefile,
    	overwrite/README, overwrite/overwrite.c, overwrite/rtems_init.c:
    	New files.

---
+++ gdb/overwrite/overwrite.c
@@ -0,0 +1,61 @@
+/*
+ *  Simple test program to demonstrate memory overwrite.
+ *
+ *  $Id$
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+typedef struct {
+  uint32_t    TooShortArray[8];
+  uint32_t    RandomVariable;
+  uint32_t    CorruptedVariable;
+} Struct_t;
+
+Struct_t S;
+
+void writeEntry(int index, uint32_t value);
+
+int main(
+  int    argc,
+  char **argv
+)
+{
+  puts( "\n\n*** MEMORY CORRUPTION EXAMPLE ***" );
+
+  /* initialize global variables */
+  memset( &S, 0, sizeof(S) );
+  S.RandomVariable = 0x1234;
+  S.CorruptedVariable = 0x5678;
+
+  printf( 
+    "&TooShortArray[0] = %p\n"
+    "&TooShortArray[7] = %p\n"
+    "&RandomVariable = %p\n"
+    "&CorruptedVariable = %p\n",
+    &S.TooShortArray[0],
+    &S.TooShortArray[7],
+    &S.RandomVariable,
+    &S.CorruptedVariable
+  );
+  /* write past end of array */
+  writeEntry( 8, 0xdeadf00d );
+  writeEntry( 9, 0x0badd00d );
+
+  printf( 
+    "RandomVariable = 0x%08x\n"
+    "CorruptedVariable = 0x%08x\n",
+    S.RandomVariable,
+    S.CorruptedVariable
+  );
+  puts( "*** END OF MEMORY CORRUPTION EXAMPLE ***" );
+  exit( 0 );
+}
+
+void writeEntry(int index, uint32_t value)
+{
+  S.TooShortArray[index] = value;
+}
---
+++ gdb/overwrite/rtems_init.c
@@ -0,0 +1,32 @@
+/*
+ *  Wrapper for Overwrite main().
+ *
+ *  $Id$
+ */
+
+#include <bsp.h>
+
+int main( int, char ** );
+
+rtems_task Init(
+  rtems_task_argument ignored
+)
+{
+  main( 0, NULL );
+  exit( 0 );      /* just in case */
+}
+
+/* configuration information */
+
+/* NOTICE: the clock driver is explicitly disabled */
+#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+#define CONFIGURE_MAXIMUM_TASKS 1
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+
+/* end of file */

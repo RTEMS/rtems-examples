d7a14c53b95ca2c362704b395d26dd047f503aa8
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-04-14 15:52:02 +0000

    2010-04-14	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* x86_display_cpu/.cvsignore, x86_display_cpu/Makefile,
    	x86_display_cpu/test.c: New files.

---
+++ misc/x86_display_cpu/test.c
@@ -0,0 +1,34 @@
+/*
+ *  Simple test program -- simplified version of sample test hello.
+ *
+ *  $Id$
+ */
+
+#include <bsp.h>
+#include <rtems/bspIo.h>
+#include <libcpu/cpuModel.h>
+
+rtems_task Init(
+  rtems_task_argument ignored
+)
+{
+  printk( "\n\n*** x86 DISPLAY CPU CAPABILITIES TEST ***\n" );
+  printCpuInfo();
+  printk( "\n\n*** END OF x86 DISPLAY CPU CAPABILITIES TEST ***\n" );
+  exit( 0 );
+}
+
+/* configuration information */
+
+/* NOTICE: the clock driver is explicitly disabled */
+#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+#define CONFIGURE_MAXIMUM_TASKS 1
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+
+/* end of file */

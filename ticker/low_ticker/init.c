26a8a9cdeef06ba92129c058e41549f62fd68fc5
Joel Sherrill <joel@rtems.org>
2022-02-23 16:16:58 -0600

    ticker/low_ticker: Fix warnings

--- ticker/low_ticker/init.c
+++ ticker/low_ticker/init.c
@@ -17,10 +17,9 @@ rtems_task Test_task(
 )
 {
   rtems_time_of_day time;
-  rtems_status_code status;
 
   for ( ; ; ) {
-    status = rtems_clock_get_tod( &time );
+    (void) rtems_clock_get_tod( &time );
     if ( time.second >= 35 ) {
       printk( "*** END OF LOW MEMORY CLOCK TICK TEST ***\n" );
       rtems_shutdown_executive( 0 );
@@ -29,7 +28,7 @@ rtems_task Test_task(
       task_index, " - rtems_clock_get_tod - ", 
       (int) time.hour, (int) time.minute, (int) time.second,
       (int) time.month, (int) time.day, (int) time.year );
-    status = rtems_task_wake_after(
+    (void) rtems_task_wake_after(
       task_index * 5 * rtems_clock_get_ticks_per_second()
     );
   }
@@ -39,7 +38,6 @@ static void *Init(
   uintptr_t ignored
 )
 {
-  rtems_status_code status;
   rtems_time_of_day time;
   rtems_id          id;
   int               i;
@@ -54,18 +52,21 @@ static void *Init(
   time.second = 0;
   time.ticks  = 0;
 
-  status = rtems_clock_set( &time );
+  (void) rtems_clock_set( &time );
 
   for (i=1 ; i<=3 ; i++ ) {
-    status = rtems_task_create(
+    (void) rtems_task_create(
       rtems_build_name( 'T', 'A', 0x30+1, ' ' ), 1, 0, RTEMS_DEFAULT_MODES,
       RTEMS_DEFAULT_ATTRIBUTES, &id
     );
-    status = rtems_task_start( id, Test_task, i );
+    (void) rtems_task_start( id, Test_task, i );
   }
 
-  while( 1 )
+  while( 1 ) {
     ;
+  }
+
+  return NULL;
 }
 
 /**************** START OF CONFIGURATION INFORMATION ****************/
@@ -79,7 +80,7 @@ static void *Init(
 #define CONFIGURE_MINIMUM_TASK_STACK_SIZE 512
 #define CONFIGURE_MAXIMUM_PRIORITY 15
 #define CONFIGURE_DISABLE_CLASSIC_API_NOTEPADS
-#define CONFIGURE_IDLE_TASK_BODY Init
+#define CONFIGURE_IDLE_TASK_BODY (Thread_Idle_body)Init
 #define CONFIGURE_IDLE_TASK_INITIALIZES_APPLICATION
 
 #define CONFIGURE_MAXIMUM_TASKS             3

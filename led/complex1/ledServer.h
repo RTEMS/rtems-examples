2e26fe340314e3e18fd61f50acc1204496f90fec
Joel Sherrill <joel@rtems.org>
2026-07-29 10:12:57 -0500

    led/complex1: Clean up and verify runs
    This includes some minor fixes but mostly clean up.

--- led/complex1/ledServer.h
+++ led/complex1/ledServer.h
@@ -1,14 +1,43 @@
-//
-//
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/*
+ * COPYRIGHT (c) 2026.
+ * On-Line Applications Research Corporation (OAR).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef __LED_SERVER_h
 #define __LED_SERVER_h
 
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
+
 #include "MessageQueueT.h"
 
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
 #include <time.h>
 
 #include "../led.h"
@@ -120,8 +149,8 @@ namespace Examples {
         }
         rc = ::pthread_setname_np( mThread, "LEDs" );
         if ( rc ) {
-          std::cerr << "LEDServer: pthread_setname_np failed "
-                    << status << std::endl;
+          std::cerr << "LEDServer: pthread_setname_np failed: "
+                    << strerror(rc) << std::endl;
         }
       }
 
